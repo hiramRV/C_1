cbe580be0ac2258b7a66bf95b58e56fc01c22c59
HiramRubio <stevenrv97@gmail.com>
2020-09-07 02:47:13 -0600

    Update
    - Agregando gitignore
    - Tareas de la semana 1

---
+++ S1/HelloWorld.c
@@ -0,0 +1,11 @@
+/* Tarea semana 1
+Steven Rubio
+*/
+
+#include<stdio.h>
+
+int main(void)
+{   
+    printf("Hola mundo! (Hello world in spanish)\n");
+    return 0;
+}   
---
+++ S1/Poem.c
@@ -0,0 +1,15 @@
+/*  Tarea 2, semana 1
+	Steven Rubio
+	Poema
+*/
+
+#include<stdio.h>
+
+int main(void)
+{   
+	//Spanish Poem
+	printf("\n\nUna rosa es una flor,\nun tesoro es una fortuna \ny a alguien como tu, \nno la cambio por ninguna.\n\n");
+	//Some ASCII Art from https://www.asciiart.eu/plants/flowers
+	printf("   vVVVv \n   (___) \n    ~Y~ \n    /|  \n   //|//\n  ^^^^^^^\n");
+    return 0;
+}   
